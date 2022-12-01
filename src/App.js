6c592465a9cd479101274fd2398109d3a423a7af
sahil patle <89509445+codewisersahil@users.noreply.github.com>
2022-12-01 22:19:40 +0530

    new

---
+++ src/App.js
@@ -0,0 +1,17 @@
+// import logo from './logo.svg';
+import React from 'react';
+import './App.css';
+import Home from './components/Home';
+import Homecontanier from './contaniers/Homecontanier'
+
+function App() {
+  return (
+    <div className="App">
+      <Homecontanier />
+      <Home />
+      
+    </div>
+  );
+}
+
+export default App;
